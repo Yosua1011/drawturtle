0bc07159d1bf65a75c4788b080187c66d19bf453
Yosua1011 <yosua1011@gmail.com>
2016-10-03 10:29:46 +0700

    Optimalization draw a square process using loop

--- mindstorms.py
+++ mindstorms.py
@@ -1,23 +1,20 @@
 import turtle
 
 def draw_square():
+	for i in range(1,5):
+		some_turtle.forward(100)
+		some_turtle.right(90)
+
+def draw_art():
 	window = turtle.Screen()
 	window.bgcolor("red")
-
+	#Create the turtle Brad - Draws a square
 	brad = turtle.Turtle()
 	brad.shape("turtle")
 	brad.color("yellow")
 	brad.speed(2)
-
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
-
+	draw_square(brad)
+	#Create the turtle Angie - Draws a circle
 	angie = turtle.Turtle()
 	angie.shape("arrow")
 	angie.color("blue")
