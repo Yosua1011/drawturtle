bad3051aaceab88392127692bcad5351fe9b541f
Yosua1011 <yosua1011@gmail.com>
2016-10-03 11:11:23 +0700

    Fix function call to draw_art Fix argument for draw_square function to some_turtle Make Draws a circle lines into a comment

--- mindstorms.py
+++ mindstorms.py
@@ -1,6 +1,6 @@
 import turtle
 
-def draw_square():
+def draw_square(some_turtle):
 	for i in range(1,5):
 		some_turtle.forward(100)
 		some_turtle.right(90)
@@ -17,11 +17,11 @@ def draw_art():
 		draw_square(brad)
 		brad.right(10)
 	#Create the turtle Angie - Draws a circle
-	angie = turtle.Turtle()
-	angie.shape("arrow")
-	angie.color("blue")
-	angie.circle(100)
+	#angie = turtle.Turtle()
+	#angie.shape("arrow")
+	#angie.color("blue")
+	#angie.circle(100)
 
 	window.exitonclick()
 	
-draw_square()
+draw_art()
