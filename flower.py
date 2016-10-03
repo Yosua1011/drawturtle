3f46a08caaae8b975dda41f216cf97c348f9a0e8
Yosua1011 <yosua1011@gmail.com>
2016-10-03 13:37:41 +0700

    Change to rhombus instead square and add two leaves to the flower

--- flower.py
+++ flower.py
@@ -1,27 +1,37 @@
 import turtle
 
-def draw_square(some_turtle):
-	for i in range(1,5):
-		some_turtle.forward(100)
-		some_turtle.right(90)
-		
-def draw_art():
-	window = turtle.Screen()
-	window.bgcolor("green")
+def draw_rhombus(some_turtle,length,angle):
+	for i in range(1,3):
+		some_turtle.forward(length)
+		some_turtle.left(180-angle)
+		some_turtle.forward(length)
+		some_turtle.left(angle)
 
-	#Create the turtle Brad - Draws a Square to make a flower
+def draw_shapes():
+	
+	window = turtle.Screen()
+	window.bgcolor("white")
+
 	brad = turtle.Turtle()
-	brad.shape("turtle")
-	brad.color("red")
-	brad.speed(15)
-	for i in range(1,37):
-		draw_square(brad)
+	brad.shape("arrow")
+	brad.color("blue")
+	brad.speed(30)
+
+	for i in range(1,73):
+		draw_rhombus(brad,100,140)
 		brad.right(10)
 
-	for i in range(37,38):		
-		brad.left(90)
-		brad.backward(350)
+	brad.right(90)
+	brad.forward(300)
+		
+	brad.right(145)
+	draw_rhombus(brad,150,140)
+		
+	brad.right(110)
+	draw_rhombus(brad,150,140)
+
+	brad.hideturtle()
 
 	window.exitonclick()
 
-draw_art()
+draw_shapes()
