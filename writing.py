582c071a807b7502fca88c4d714a07405186c4f9
Yosua1011 <Yosua1011@gmail.com>
2016-10-11 10:25:15 +0700

    Create a file to write a text using Python

---
+++ writing.py
@@ -0,0 +1,11 @@
+import turtle
+
+def draw_messi():
+	window = turtle.Screen()
+	window.bgcolor("white")
+
+	turtle.write("messi fan", font=("Arial", 40, "normal"))
+
+	window.exitonclick()
+
+draw_messi()
